1b1443ff5bb222448a850439f2114bf8514e19e0
dvasilchuk <dvasilchuk@phoenixit.ru>
2023-09-18 21:06:40 +0300

    add table total

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/ru-RU';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/ru-RU";
 
 Vue.use(ElementUI, { locale });
-
 
 // const router = Router.createRouter({
 //   routes: [
@@ -21,7 +20,6 @@ Vue.use(ElementUI, { locale });
 //   mode: 'history'
 // });
 // router.replace('/main');
-
 
 Vue.config.productionTip = false;
 
---
+++ src/pages/scripts/CalculationTableTotal/CalculationTableTotal.vue
@@ -0,0 +1,90 @@
+<script>
+import { table, columns, total, totalDataConst } from "./consts";
+import { sumBy, toNumber, toString, reduce } from "lodash";
+export default {
+  name: "TestTable",
+  data() {
+    return {
+      table,
+      columns,
+      total,
+      totalDataConst
+    };
+  },
+  methods: {},
+  computed: {
+    isShowTotal() {
+      return !!this.total;
+    },
+    totalDataComputed() {
+      const stringToNumber = item =>
+        toNumber(item[this.total.id].replace(" ", ""));
+      const summaryString = data =>
+        toString(sumBy(data, item => stringToNumber(item)));
+
+      const groupTotal = () =>
+        reduce(
+          this.table,
+          (acc, current) => {
+            if (!acc[current[this.total.groupBy]]) {
+              acc = {
+                ...acc,
+                [current[this.total.groupBy]]: stringToNumber(current)
+              };
+            } else {
+              acc = {
+                ...acc,
+                [current[this.total.groupBy]]:
+                  acc[current[this.total.groupBy]] + stringToNumber(current)
+              };
+            }
+            return acc;
+          },
+          {}
+        );
+
+      return this.columns.map(({ key }, index) => {
+        if (index === 0) return { id: key, value: "Итого" };
+        if (key === this.total.id)
+          return {
+            id: key,
+            value: !this.total.groupBy
+              ? summaryString(this.table)
+              : groupTotal()
+          };
+        return { id: key, value: null };
+      });
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <!--    Заголовки-->
+    <div class="row" style="font-weight: bold">
+      <div v-for="item in columns" :key="item.id" class="col-sm">
+        {{ item.label }}
+      </div>
+    </div>
+
+    <!--    Таблица-->
+    <div v-for="(row, i) in table" :key="i" class="row">
+      <div v-for="item in columns" :key="item.id" class="col-sm">
+        {{ row[item.key] }}
+      </div>
+    </div>
+
+    <!--    Итог-->
+    <div v-for="(row, i) in totalDataComputed" :key="i" class="row">
+      {{ row.value }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
